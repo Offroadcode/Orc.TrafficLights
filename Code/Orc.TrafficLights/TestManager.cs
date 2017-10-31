e801f33ab90d09900582d761d5cc4e8ef1b9f624
Jack Penney <J.s.penney@hotmail.co.uk>
2017-10-31 16:23:45 +0000

    Updated Tests to run as async

--- Code/Orc.TrafficLights/TestManager.cs
+++ Code/Orc.TrafficLights/TestManager.cs
@@ -2,6 +2,7 @@
 using Orc.TrafficLights.Tests;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics.Contracts;
 using System.Linq;
 using System.Reflection;
 using System.Text;
@@ -11,6 +12,8 @@ namespace Orc.TrafficLights
 {
     public static class TestManager
     {
+        private static List<TestResult> results = null;
+
         /// <summary>
         /// backing variable for the list of tests
         /// </summary>
@@ -26,7 +29,7 @@ namespace Orc.TrafficLights
                 if (_tests == null)
                 {
                     _tests = new List<BaseTrafficLightTest>();
-                    
+
                     //Get a type object for our base
                     Type ti = typeof(BaseTrafficLightTest);
 
@@ -52,25 +55,73 @@ namespace Orc.TrafficLights
 
         public static List<TestResult> RunTests()
         {
-            List<TestResult> results = new List<TestResult>();
+            results = new List<TestResult>();
+
+            var tasks = new List<Task>();
             foreach (var test in Tests)
             {
-                var model = new TestResult();
-                model.Name = test.TestName;
-                model.Status = TestStatus.Unknown;
-                try
+                // Create an async task for the test
+                var task = Task.Factory.StartNew(() => 
                 {
-                    test.RunTest(ref model);
-                }
-                catch (Exception e)
-                {
-                    model.Status = TestStatus.Exception;
-                    model.Message = e.Message;
-                }
-                results.Add(model);
+                    RunTest(test);
+                });
+
+                // Add to our list of tasks
+                tasks.Add(task);
             }
+
+            // Wait for all tasks to complete before we return the results
+            Task.WaitAll(tasks.ToArray());
+
             return results;
         }
+
+        public static TestResult RunTest(BaseTrafficLightTest test)
+        {
+            var model = new TestResult();
+            model.Name = test.TestName;
+            model.Status = TestStatus.Unknown;
+
+            try
+            {
+                test.RunTest(ref model);
+            }
+            catch (Exception e)
+            {
+                model.Status = TestStatus.Exception;
+                model.Message = e.Message;
+            }
+
+            results.Add(model);
+            return model;
+        }
+
+        //public static async Task<TestResult> RunTestAsync(BaseTrafficLightTest test)
+        //{
+        //    var model = new TestResult();
+        //    model.Name = test.TestName;
+        //    model.Status = TestStatus.Unknown;
+
+        //    // wait for the test to complete running before we proceed with the rest of the logic
+        //    // Can't use await await Task.Run(() => as its .Net 4.5 
+        //    // await Task.Factory.StartNew(() => is for .Net 4.0
+
+        //    await Task.Run(() =>
+        //    {
+        //        try
+        //        {
+        //            test.RunTest(ref model);
+        //        }
+        //        catch (Exception e)
+        //        {
+        //            model.Status = TestStatus.Exception;
+        //            model.Message = e.Message;
+        //        }
+        //    });
+
+        //    results.Add(model);
+        //    return model;
+        //}
 
         /// <summary>
         /// Safely Retrives all loadable types from the assembly
