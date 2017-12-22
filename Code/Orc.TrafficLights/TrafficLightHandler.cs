cf99535fa6edfa911606940aee320d2e50edeaaf
Jack Penney <J.s.penney@hotmail.co.uk>
2017-12-22 10:54:47 +0000

    Updated traffic light page to not run automated tests between the specified date range

--- Code/Orc.TrafficLights/TrafficLightHandler.cs
+++ Code/Orc.TrafficLights/TrafficLightHandler.cs
@@ -22,30 +22,11 @@ namespace Orc.TrafficLights
 
         public void ProcessRequest(HttpContext context)
         {
-            var output = TestManager.RunTests();
-
-            if (context.Request.QueryString.AllKeys.Any(x => x == "automatedCheck"))
+            if (!IgnoreTests(context))
             {
-                // Read the time range we don't throw errors im between
-                // This is for sites running maintanence at certain times, so we can avoid false positive alerts
-                var startTimeString = context.Request.QueryString["disabledStartTime"];
-                var endTimeString = context.Request.QueryString["disabledEndTime"];
+                var output = TestManager.RunTests();
 
-                TimeSpan startTime;
-                TimeSpan endTime;
-                if (TimeSpan.TryParse(startTimeString, out startTime) && TimeSpan.TryParse(endTimeString, out endTime))
-                {
-                    // Don't show an error page when its thrown between the specified time range
-                    if (!(DateTime.UtcNow.TimeOfDay > startTime && DateTime.UtcNow.TimeOfDay < endTime))
-                    {
-                        if (output.Any(x => x.Status == Models.TestStatus.Failure || x.Status == Models.TestStatus.Exception))
-                        {
-                            context.Response.StatusCode = 500;
-                            context.Response.Write("Error");
-                        }
-                    }
-                }
-                else
+                if (context.Request.QueryString.AllKeys.Any(x => x == "automatedCheck"))
                 {
                     if (output.Any(x => x.Status == Models.TestStatus.Failure || x.Status == Models.TestStatus.Exception))
                     {
@@ -53,43 +34,74 @@ namespace Orc.TrafficLights
                         context.Response.Write("Error");
                     }
                 }
-            }
-            else
-            {
-
-                context.Response.Write("<html><body><h1>Traffic Lights</h1>");
-                context.Response.Write("<table>");
-                context.Response.Write("<tr>");
-                context.Response.Write("<th>Test</th>");
-                context.Response.Write("<th>Message</th>");
-                context.Response.Write("<th>Result</th>");
-                context.Response.Write("</tr>");
-                foreach (var item in output)
+                else
                 {
-                    var bgColor = "#00FF00";
-                    if (item.Status == Models.TestStatus.Failure)
-                    {
-                        bgColor = "#FF0000";
-                    }
-                    else if (item.Status == Models.TestStatus.Warning)
-                    {
-                        bgColor = "#FFFF00";
-                    }
-                    else if (item.Status == Models.TestStatus.Exception)
-                    {
-                        bgColor = "#0000FF";
-                    }
+                    context.Response.Write("<html><body><h1>Traffic Lights</h1>");
+                    context.Response.Write("<table>");
                     context.Response.Write("<tr>");
-                    context.Response.Write("<td>" + item.Name + "</td>");
-                    context.Response.Write("<td>" + item.Message + "</td>");
-                    context.Response.Write("<td bgcolor=\"" + bgColor + "\">" + item.Status + "</td>");
+                    context.Response.Write("<th>Test</th>");
+                    context.Response.Write("<th>Message</th>");
+                    context.Response.Write("<th>Result</th>");
                     context.Response.Write("</tr>");
+                    foreach (var item in output)
+                    {
+                        var bgColor = "#00FF00";
+                        if (item.Status == Models.TestStatus.Failure)
+                        {
+                            bgColor = "#FF0000";
+                        }
+                        else if (item.Status == Models.TestStatus.Warning)
+                        {
+                            bgColor = "#FFFF00";
+                        }
+                        else if (item.Status == Models.TestStatus.Exception)
+                        {
+                            bgColor = "#0000FF";
+                        }
+                        context.Response.Write("<tr>");
+                        context.Response.Write("<td>" + item.Name + "</td>");
+                        context.Response.Write("<td>" + item.Message + "</td>");
+                        context.Response.Write("<td bgcolor=\"" + bgColor + "\">" + item.Status + "</td>");
+                        context.Response.Write("</tr>");
+                    }
+                    context.Response.Write("</table>");
+                    context.Response.Write("</body></html>");
                 }
-                context.Response.Write("</table>");
-                context.Response.Write("</body></html>");
             }
         }
 
         #endregion
+
+        /// <summary>
+        /// Returns a flag for if the test should be ran or not
+        /// </summary>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        private bool IgnoreTests(HttpContext context)
+        {
+            var result = false;
+
+            // Is the test automated
+            if (context.Request.QueryString.AllKeys.Any(x => x == "automatedCheck"))
+            {
+                // get the start and end timespans for when we ignore automated tests
+                var startTimeString = context.Request.QueryString["disabledStartTime"];
+                var endTimeString = context.Request.QueryString["disabledEndTime"];
+
+                TimeSpan startTime;
+                TimeSpan endTime;
+                if (TimeSpan.TryParse(startTimeString, out startTime) && TimeSpan.TryParse(endTimeString, out endTime))
+                {
+                    // if the current time is within the ignored range
+                    if (DateTime.UtcNow.TimeOfDay > startTime && DateTime.UtcNow.TimeOfDay < endTime)
+                    {
+                        // ignore the test
+                        result = true;
+                    }
+                }
+            }
+
+            return result;
+        }
     }
 }
